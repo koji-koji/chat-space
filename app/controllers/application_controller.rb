66ec13ccb5ded5aa73fa0032dd0a97fc42952506
koji-koji <k.koyama37@gmail.com>
2017-05-04 14:22:29 +0900

    deviseの導入とuser_nameカラムの追加
    deviseを導入して、user_nameカラムの作成まで行った。
    
    ・deviseのインストール
    ・deviseモデルの作成
    ・deviseビューの作成
    ・user_nameカラムを作成してrake db:migrateを行なった。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_cotroller?
+
+  def configure_parmitted_parameters
+    devise_parameter_sanitizer.permit(: sign_up, [:user_name])
+  end
 end
