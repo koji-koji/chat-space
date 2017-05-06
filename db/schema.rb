29e4ae57b21b8363874eb6fd87191922f849043d
koji-koji <k.koyama37@gmail.com>
2017-05-06 12:27:35 +0900

    unique: trueに変更
    unique: trueにできていなかったので、マイグレーションファイルを作成して実行した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506024949) do
+ActiveRecord::Schema.define(version: 20170506032406) do
 
   create_table "chatgroups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "group_name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "group_name", default: "", null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.index ["group_name"], name: "index_chatgroups_on_group_name", unique: true, using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
