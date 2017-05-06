51018a7dd71b384d3bc40ab7feb4d146457d66fa
koji-koji <k.koyama37@gmail.com>
2017-05-06 17:53:44 +0900

    チャットグループ作成ページのformの適用
    pry-railsのインストール
    ルーティングの作成
    new.html.hamlにform_forを導入

--- app/controllers/chatgroups_controller.rb
+++ app/controllers/chatgroups_controller.rb
@@ -1,2 +1,16 @@
 class ChatgroupsController < ApplicationController
+
+  def new
+    @chatgroup = Chatgroup.new
+  end
+
+  def create
+    Chatgroup.create(group_name: params[:chatgroup][:group_name])
+  end
+
+  def edit
+  end
+
+
+
 end
