cc40874a1a4857115e6c7954d3f1d8d08e0b187e
koji-koji <k.koyama37@gmail.com>
2017-05-06 11:58:16 +0900

    カラムの設定
    group_nameカラムを追加するための記述をした。null: false, unique: trueをオプションとした。

--- db/migrate/20170506024949_create_chatgroups.rb
+++ db/migrate/20170506024949_create_chatgroups.rb
@@ -1,7 +1,7 @@
 class CreateChatgroups < ActiveRecord::Migration[5.0]
   def change
     create_table :chatgroups do |t|
-
+      t.string :group_name,null: false, unique: true
       t.timestamps
     end
   end
