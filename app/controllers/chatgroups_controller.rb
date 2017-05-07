91d799e3500bf046be959c2b10b235a916f49bec
koji-koji <k.koyama37@gmail.com>
2017-05-07 12:59:31 +0900

    flashメッセージの表示、ストロングパラメータの表記
    ・chatgroup.rbにバリデーションの記述。
    ・index.hamlからapplication.html.haml
    にflashメッセージの記述を移し替え。
    ・chatgroup_contollerにストロングパラメータの記述。
    ・chatgroup_contollerにflashメッセージを表示するように記述。ログインとログアウト時にそれぞれ別のメッセージを表示するよう
    にした。

--- app/controllers/chatgroups_controller.rb
+++ app/controllers/chatgroups_controller.rb
@@ -5,10 +5,23 @@ class ChatgroupsController < ApplicationController
   end
 
   def create
-    Chatgroup.create(group_name: params[:chatgroup][:group_name])
+
+    chatgroup = Chatgroup.new(group_name: params[:chatgroup][:group_name])
+
+    if chatgroup.save(group_params)
+      redirect_to root_path, notice: "グループ作成完了！"
+    else
+      redirect_to new_chatgroup_path, alert: "グループ作成失敗"
+    end
   end
 
   def edit
   end
 
+  private
+  def group_params
+    params.require(:chatgroup).permit(:name)
+  end
+
 end
+
--- app/models/chatgroup.rb
+++ app/models/chatgroup.rb
@@ -1,2 +1,4 @@
 class Chatgroup < ApplicationRecord
+ validates_presence_of :group_name
+ validates :group_name, uniqueness: true
 end
