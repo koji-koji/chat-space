b521ea004f25e03b4d20bfa7280945575b9bd13f
koji-koji <k.koyama37@gmail.com>
2017-05-11 11:37:35 +0900

    ルーティングの修正
    アクションを誤ってpostとしていたため、createに修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
-  resources :chatgroups, except: [:index ,:show] do
-    resources :comments, only: [:index ,:post]
+  root 'comments#index'
+  resources :chatgroups, except: [:index, :show] do
+    resources :comments, only: [:index, :create]
   end
 end
