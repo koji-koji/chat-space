c3bfd69732985fb9e7610b4a11bcd536299fbb14
koji-koji <k.koyama37@gmail.com>
2017-05-11 13:28:42 +0900

    createメソッドの記述
    createメソッドを記述し、ストロングパラメーターも導入した。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,17 @@ class CommentsController < ApplicationController
     @chatgroup = Chatgroup.find(params[:chatgroup_id])
     @comment = Comment.new
   end
+
+  def create
+    Comment.create(comment_params)
+  end
+
+  private
+
+  def comment_params
+    params[:user_id] = current_user.id
+    params[:comment] = params[:comment][:comment]
+    params.permit(:chatgroup_id, :user_id, :comment)
+  end
 end
+
