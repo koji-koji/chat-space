c035bf2b244271958a633029b18f38f22720e09d
koji-koji <k.koyama37@gmail.com>
2017-05-11 11:34:31 +0900

    commentsに対するアソシエーションの設定
    commentsに対するアソシエーションの設定

--- app/models/chatgroup.rb
+++ app/models/chatgroup.rb
@@ -2,5 +2,6 @@ class Chatgroup < ApplicationRecord
   validates :group_name, uniqueness: true, presence: true
   has_many :users, through: :chatgroup_users
   has_many :chatgroup_users
+  has_many :comments
   accepts_nested_attributes_for :chatgroup_users
 end
