6e83defe8cb83bee38773300cb403b4180a0a9b5
koji-koji <k.koyama37@gmail.com>
2017-05-11 18:48:21 +0900

    accepts_nested_attributes_forの削除
    accepts_nested_attributes_forの削除

--- app/models/chatgroup.rb
+++ app/models/chatgroup.rb
@@ -3,5 +3,4 @@ class Chatgroup < ApplicationRecord
   has_many :users, through: :chatgroup_users
   has_many :chatgroup_users
   has_many :comments
-  accepts_nested_attributes_for :chatgroup_users
 end
