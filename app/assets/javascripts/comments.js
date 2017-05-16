71885aaa326d3849e9524b0c7c03e3c998cdd165
koji-koji <k.koyama37@gmail.com>
2017-05-16 13:23:30 +0900

    jbuilderの値の利用とレイアウト調整
    ・jbuilderの値を受け取るように調整
    ・コメント、名前、投稿時間のレイアウト調整
    ・ajaxのurlが仮設定だったため、どのidとも対応するように修正
    ・デバックの際に使用したconsole.logの削除

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,18 +1,15 @@
 $(function() {
-  function buildHTML(comments) {
-    var html = $('<li class="comments">').append('comments');
+  function buildHTML(data) {
+    var html = "<div class = 'chat-contents__body__content__line clearfix'>" + "<div class = 'chat-contents__body__content__line__name'>"+ data.name + "</div>" + "<div class = 'chat-contents__body__content__line__time'>" + data.time + "</div>" + "</div>" + "<div class = 'chat-contents__body__content__message'>" + data.comment + "</div>"
     return html;
   }
   $('.chat-contents__type-message__button').on('click', function(e) {
     e.preventDefault();
     var textField = $('.chat-contents__type-message__box__message');
     var comment = textField.val();
-    console.log(comment);
-    var test =  {comments: { comment: comment}};
-    console.log(test);
     $.ajax({
       type: 'POST',
-      url: '/chatgroups/106/comments',
+      url: window.location.href,
       data: {
         comments: {
           comment: comment
@@ -22,11 +19,11 @@ $(function() {
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.comments').append(html);
+      var show = $('<li class="comment" style = "list-style: none;">').append(html)
+      $('.chat-contents__body__comments').append(show)
       textField.val('')
     })
     .fail(function(error){
-      console.log(error);
       alert('error');
     });
   });
