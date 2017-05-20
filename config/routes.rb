c0562670655e9bd51fb62d15ee4b615824cb8cfe
koji-koji <k.koyama37@gmail.com>
2017-05-20 19:33:01 +0900

    インクリメンタルサーチのためのルーティングの設定
    インクリメンタルサーチのためのルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'chatgroups#new'
   resources :chatgroups, except: [:index, :show] do
+    collection do
+      get 'search'
+    end
     resources :comments, only: [:index, :create]
   end
 end
