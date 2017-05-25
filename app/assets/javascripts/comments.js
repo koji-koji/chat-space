8f487fdf5322081eaa7ee32375ad4e08f5a1e95e
koji-koji <k.koyama37@gmail.com>
2017-05-25 15:05:39 +0900

    comments.jsの調整
    コメントの非同期通信がうまくいっていなかったので調整した。
    ・画像も遅れるように
    ・非同期通信のhtmlの簡略化
    
    自動更新の調整
    ・自動更新できるようにした。
    ・コメントがゼロのときで自動更新を行うように調整をした。

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,28 +1,38 @@
 $(function() {
   function buildHTML(data) {
-    var html = `<div class = 'chat-contents__body__content__line clearfix'>
-                  <div class = 'chat-contents__body__content__line__name' data-comment-id="${data.comment.id}>
-                    ${data.name}
-                  </div>
-                  <div class = 'chat-contents__body__content__line__time' >
-                    ${data.time}
-                  </div>
-                </div>
-                <div class = 'chat-contents__body__content__message'>
-                  ${data.comment}
-                </div>`
-    return html;
+    var html =  `
+      <li class="comment" style = "list-style: none;">
+        <div class = 'chat-contents__body__content__line clearfix'>
+          <div class = 'chat-contents__body__content__line__name'>
+            ${data.name}
+          </div>
+          <div class = 'chat-contents__body__content__line__time'>
+            ${data.time}
+          </div>
+        </div>
+        <div class = 'chat' data-comment-id = '${data.id}'>
+        </div>
+        <div class = 'chat-contents__body__content__message'>
+            ${data.comment}
+        </div>`
+      console.log(data.id)
+      if (data.image.url == null){
+        html = $(html).append(`</li>`)
+      } else {
+        html = $(html).append(`<div class = 'chat-contents__body__content__image'><img src = '${data.image.url}' width="256" height="256"}</div>
+      </li>`)
+      }
+    $('.chat-contents__body__comments').append(html)
   }
 
-  function appendHTML(data) {
-    var html = buildHTML(data);
-    var show = $('<li class="comment" style = "list-style: none;">').append(html)
-    $('.chat-contents__body__comments').append(show)
-  }
 
   function countup() {
-    var Id = document.querySelectorAll( "[data-comment-id]" )
-    var lastId = $(Id[Id.length-1]).attr('data-comment-id')
+    if ($('.chat').last(0).data('commentId') == null ){
+      var lastId = 0
+    } else {
+      var lastId = $('.chat').last(0).data('commentId')
+    }
+    console.log(lastId)
     $.ajax({
       type: 'GET',
       url: window.location.href,
@@ -32,34 +42,33 @@ $(function() {
       dataType: 'json'
     })
     .done(function(data) {
-      $.each(data.comments, function(i,data){
-        appendHTML(data)
+      $.each(data.comments, function(i, data){
+        buildHTML(data)
       })
     });
   }
-  setInterval(countup, 5000);
+  setInterval(countup, 10000);
 
-  $('.chat-contents__type-message__button').on("submit", function(e) {
+
+  $('.form').on("submit", function(e) {
     e.preventDefault();
-    var textField = $('.chat-contents__type-message__box__message');
-    var comment = textField.val();
-    if ( event.target.src != nill ) {
-      var image = event.target.src;
-    }
+    console.log(this)
+    var formdata = new FormData($(this).get(0));
     $.ajax({
       type: 'POST',
       url: window.location.href,
-      data: {
-        comments: {
-          comment: comment,
-          image: image
-        }
-      },
-      dataType: 'json'
+      data: formdata,
+      dataType: 'json',
+      contentType: false,
+      processData: false,
+      disabled: false
     })
     .done(function(data){
-      appendHTML(data)
-      textField.val('')
+      console.log("hoge")
+      buildHTML(data);
+      var textField = $('.chat-contents__type-message__box__message')
+      $(textField).val('')
+      $('.chat-contents__type-message__button').removeAttr("disabled");
       alert('送信が成功しました')
     })
     .fail(function(error){
