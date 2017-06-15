52ae357fbee3b2a266985d4369cbbabcdef627ad
koji-koji <k.koyama37@gmail.com>
2017-06-15 18:15:07 +0900

    user_search_controllerの作成
    rails g controller user_searchの実行

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,12 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     user.update(params[:current_password])
   end
+
+    def search
+    binding.pry
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.json { render 'chatgroups', handlers: 'jbuilder' }
+    end
+  end
 end
