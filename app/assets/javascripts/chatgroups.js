3ec8e5b2a47e22d61af2f11caf5d62b68e41b8b5
koji-koji <k.koyama37@gmail.com>
2017-06-15 15:03:38 +0900

    記述の調整
    シングルクウォーテーションの位置が誤っていたので、調整した。

--- app/assets/javascripts/chatgroups.js
+++ app/assets/javascripts/chatgroups.js
@@ -20,7 +20,7 @@ $(function(){
     $.ajax({
       type: 'GET',
       url: '/chatgroups/search',
-      data: ('keyword'= + input),
+      data: ('keyword=' + input),
       dataType: 'json'
     })
     .done(function(data) {
