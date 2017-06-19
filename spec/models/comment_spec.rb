f30ccb219fa6f4be0a942c4799a7623ae61f3552
koji-koji <k.koyama37@gmail.com>
2017-06-19 19:15:23 +0900

    テストの記述の調整
    テストは成功していないが、不具合を発見したので、一時的に保存した。
    
    不具合の内容は、テキストと画像を同時に保存することはできないが、画像のみだと保存ができなかったこと。

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe Comment do
+  describe '#create' do
+
+    it "is valid with a comment" do
+      comment = build(:comment, image: nil)
+      expect(comment).to be_valid
+    end
+
+    it "is valid with a image" do
+      comment = build(:comment, comment: nil)
+      expect(comment).to be_valid
+    end
+
+    it "is valid with a comment and a image" do
+      comment = build(:comment)
+      expect(comment).to be_valid
+    end
+
+    it "is invalid without a comment and a image" do
+      comment = build(:comment,comment: nil ,image: nil)
+      comment.valid?
+      expect(comment.errors[:comment,:image]).to include("can't be blank")
+    end
+
+
+    it "is invalid without a group_id" do
+      comment = build(:comment,group_id: nil)
+      comment.valid?
+      expect(comment.errors[:group_id]).to include("can't be blank")
+    end
+
+    it "is invalid without a user_id" do
+      comment = build(:comment,user_id: nil)
+      comment.valid?
+      expect(comment.errors[:user_id]).to include("can't be blank")
+    end
+
+  end
+end
