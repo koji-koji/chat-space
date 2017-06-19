451445a6699f55ed86533d952ab5af210341eb4d
koji-koji <k.koyama37@gmail.com>
2017-06-19 21:02:20 +0900

    バリデーションの調整
    以前のバリデーションは間違った記述をしていたので、修正をした。
    
    image_upload?内のfileはbinding.pryをしてimageを調べたときに画像の名前が入っていた値。

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,11 @@
 class Comment < ApplicationRecord
-  validates :comment,presence: true ,if: :image_upload?
+  validates :comment, presence: true, if: :image_upload?
   belongs_to :user
   belongs_to :chatgroup
   mount_uploader :image, ImageUploader
 
   def image_upload?
-    :image == nil
+    image.file == nil
   end
+
 end
