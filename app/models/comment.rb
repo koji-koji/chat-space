c89b8c7a60756432a552aebceff0444f8ccb4765
koji-koji <k.koyama37@gmail.com>
2017-06-19 19:18:47 +0900

    バリデーションの調整
    条件分岐を設定した。
    
    画像がないときだけ、コメントが空のバリデーションが効くようにした。

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,10 @@
 class Comment < ApplicationRecord
-  validates :comment,presence: true
+  validates :comment,presence: true ,if: :image_upload?
   belongs_to :user
   belongs_to :chatgroup
   mount_uploader :image, ImageUploader
+
+  def image_upload?
+    :image == nil
+  end
 end
