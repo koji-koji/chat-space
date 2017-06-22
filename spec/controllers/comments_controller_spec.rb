653ed8ad6f0762bc6ea1cf22b34f9e32dc3d9345
koji-koji <k.koyama37@gmail.com>
2017-06-23 04:13:18 +0900

    user loginの設定

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -5,18 +5,23 @@ describe CommentsController do
     let(:user) { create(:user)}
     let(:group) { create(:chatgroup)}
     let(:comments) { create(:comments)}
+    context 'user log_in' do
+      before do
+        login_user user
+      end
 
-    it "assigns the requested comment to @comment" do
-      comment = create(:comment)
-      get :index, id: comment
-      expect(assigns(:comment)).to eq comment
-    end
+      it "assigns the requested comment to @comment" do
+        comment = create(:comment)
+        get :index, id: comment
+        expect(assigns(:comment)).to eq comment
+      end
 
 
-    it "renders the :index template" do
-      get :index, id: comment
-      # id: 1は渡すパラメータのハッシュ
-      expect(response).to render_template :index
+      it "renders the :index template" do
+        get :index, id: comment
+        # id: 1は渡すパラメータのハッシュ
+        expect(response).to render_template :index
+      end
     end
   end
   # describe 'POST #create' do
