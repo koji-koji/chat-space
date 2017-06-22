45331e0185909117f7d0913f13b81e3d3233d2ee
koji-koji <k.koyama37@gmail.com>
2017-06-22 20:12:49 +0900

    テスト結果の調整

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 describe Comment do
   describe '#create' do
+    let(:user) { User.new(name: 'Taro', email: 'taro@example.com') }
+    let(:chatgroup) { Chatgroup.new(group_name: 'HogeHoge',)}
 
     it "is valid with a comment" do
       comment = build(:comment, image: nil)
@@ -19,21 +21,22 @@ describe Comment do
     end
 
     it "is invalid without a comment and a image" do
+
       comment = build(:comment,comment: nil ,image: nil)
       comment.valid?
-      expect(comment.errors[:image]).to include("can't be blank")
+      expect(comment.errors.messages[:comment]).to include("入力してください")
     end
 
     it "is invalid without a chatgroup_id" do
       comment = build(:comment,chatgroup_id: nil)
       comment.valid?
-      expect(comment.errors[:chatgroup_id]).to include("can't be blank")
+      expect(comment.errors.messages[:chatgroup_id]).to include("入力してください")
     end
 
     it "is invalid without a user_id" do
       comment = build(:comment,user_id: nil)
       comment.valid?
-      expect(comment.errors[:user_id]).to include("can't be blank")
+      expect(comment.errors.messages[:user_id]).to include("入力してください")
     end
 
   end
