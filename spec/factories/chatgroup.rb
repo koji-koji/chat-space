d36fc670fa7f2eb873161bbe5fe2bf84bec81639
koji-koji <k.koyama37@gmail.com>
2017-06-23 03:00:11 +0900

    factory_girlのchatgroup設定

---
+++ spec/factories/chatgroup.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+
+  factory :chatgroup do
+    group_name         {Faker::StarWars.droid}
+    created_at         Faker::Time.between(2.days.ago, Date.today, :midnight)
+    updated_at         Faker::Time.between(2.days.ago, Date.today, :midnight)
+  end
+
+end
