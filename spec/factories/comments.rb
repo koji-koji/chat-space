b1127d12b3ba3b025b93373f0337bb7414efed12
koji-koji <k.koyama37@gmail.com>
2017-06-23 03:00:51 +0900

    factory_girlのcomment設定

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -5,6 +5,8 @@ FactoryGirl.define do
     chatgroup_id      "1"
     user_id           "1"
     image              { File.open("#{Rails.root}/test/fixtures/files/test.jpg") }
+    created_at         Faker::Time.between(2.days.ago, Date.today, :midnight)
+    updated_at         Faker::Time.between(2.days.ago, Date.today, :midnight)
   end
 
 end
