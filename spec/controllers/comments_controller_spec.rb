1283a6ff0e86e8b540f44f1fe7d417c48a06eb4f
koji-koji <k.koyama37@gmail.com>
2017-06-23 02:59:28 +0900

    indexのテストの一時保存
    ユーザー、チャットグループ、コメントの作成
    
    ※デバイスのテストについて確認が必要なので、保留

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,6 +1,27 @@
 require 'rails_helper'
 
 describe CommentsController do
+  describe 'GET #index' do
+    let(:user) { create(:user)}
+    let(:group) { create(:chatgroup)}
+    let(:comments) { create(:comments)}
 
+    it "assigns the requested comment to @comment" do
+      comment = create(:comment)
+      get :index, id: comment
+      expect(assigns(:comment)).to eq comment
+    end
+
+
+    it "renders the :index template" do
+      get :index, id: comment
+      # id: 1は渡すパラメータのハッシュ
+      expect(response).to render_template :index
+    end
+  end
+  # describe 'POST #create' do
+  #   it "assigns the requested comment to @comment"
+  #   expect(assigns(:commetn)).to eq comment
+  # end
 
 end
