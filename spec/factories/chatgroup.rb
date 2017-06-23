e1f75b1ead819bbc97305ac760bbf0b5885d7650
koji-koji <k.koyama37@gmail.com>
2017-06-23 19:45:37 +0900

    factory :chatgroupの調整

--- spec/factories/chatgroup.rb
+++ spec/factories/chatgroup.rb
@@ -2,8 +2,8 @@ FactoryGirl.define do
 
   factory :chatgroup do
     group_name         {Faker::StarWars.droid}
-    created_at         Faker::Time.between(2.days.ago, Date.today, :midnight)
-    updated_at         Faker::Time.between(2.days.ago, Date.today, :midnight)
+    created_at         {Faker::Time.between(2.days.ago, Date.today, :midnight)}
+    updated_at         {Faker::Time.between(2.days.ago, Date.today, :midnight)}
   end
 
 end
