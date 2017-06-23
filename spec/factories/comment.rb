9b45f11472b535e6a24f557234b87539f8b86b26
koji-koji <k.koyama37@gmail.com>
2017-06-23 20:40:55 +0900

    時間の調整
    created_atとupdate_atの順番が逆にならないように

--- spec/factories/comment.rb
+++ spec/factories/comment.rb
@@ -5,8 +5,8 @@ FactoryGirl.define do
     chatgroup_id       { 1 }
     user_id            { 1 }
     image              { File.open("#{Rails.root}/test/fixtures/files/test.jpg") }
-    created_at         {Faker::Time.between(2.days.ago, Date.today, :midnight)}
-    updated_at         {Faker::Time.between(2.days.ago, Date.today, :midnight)}
+    created_at         Faker::Time.between(DateTime.now - 10, DateTime.now - 5)
+    updated_at         Faker::Time.between(DateTime.now - 4, DateTime.now)
     association :chatgroup
     association :user
   end
