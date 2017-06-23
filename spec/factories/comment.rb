3cbfb3eabdef85309cad4fd70b50a1e344151f14
koji-koji <k.koyama37@gmail.com>
2017-06-23 19:45:06 +0900

    factory commentの変更
    ・名前の調整
    ・記述の調整

---
+++ spec/factories/comment.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+
+  factory :comment do
+    comment            { Faker::Lorem.sentence }
+    chatgroup_id       { 1 }
+    user_id            { 1 }
+    image              { File.open("#{Rails.root}/test/fixtures/files/test.jpg") }
+    created_at         {Faker::Time.between(2.days.ago, Date.today, :midnight)}
+    updated_at         {Faker::Time.between(2.days.ago, Date.today, :midnight)}
+    association :chatgroup
+    association :user
+  end
+
+end
