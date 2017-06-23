795b8e1566e6af7091d70b203efc354dea52c54a
koji-koji <k.koyama37@gmail.com>
2017-06-23 22:04:49 +0900

    記述の調整
    ・不要な{ }の削除
    ・created_at とupdated_atの設定

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,7 +4,9 @@ FactoryGirl.define do
     pass = Faker::Internet.password
     email              {Faker::Internet.email}
     name               {Faker::Name.name }
-    password           {pass}
-    password_confirmation  {pass}
+    password           pass
+    password_confirmation  pass
+    created_at         Faker::Time.between(10.days.ago, 6.days.ago, :day)
+    updated_at         Faker::Time.between(5.days.ago , 2.days.ago, :day)
   end
 end
