699c921ba050f6d829bfaade6a5db9118d85d703
koji-koji <k.koyama37@gmail.com>
2017-06-23 11:29:22 +0900

    テストの条件の調整
    ・userとchatgroupの設定
    ・文言の調整

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe Comment do
   describe '#create' do
-    let(:user) { User.new(name: 'Taro', email: 'taro@example.com') }
-    let(:chatgroup) { Chatgroup.new(group_name: 'HogeHoge',)}
+    let(:user) {create(:user)}
+    let(:chatgroup) {create(:chatgroup)}
 
     it "is valid with a comment" do
       comment = build(:comment, image: nil)
@@ -24,19 +24,19 @@ describe Comment do
 
       comment = build(:comment,comment: nil ,image: nil)
       comment.valid?
-      expect(comment.errors.messages[:comment]).to include("入力してください")
+      expect(comment.errors.messages[:comment]).to include("を入力してください")
     end
 
     it "is invalid without a chatgroup_id" do
       comment = build(:comment,chatgroup_id: nil)
       comment.valid?
-      expect(comment.errors.messages[:chatgroup_id]).to include("入力してください")
+      expect(comment.errors.messages[:chatgroup_id]).to include("を入力してください")
     end
 
     it "is invalid without a user_id" do
       comment = build(:comment,user_id: nil)
       comment.valid?
-      expect(comment.errors.messages[:user_id]).to include("入力してください")
+      expect(comment.errors.messages[:user_id]).to include("を入力してください")
     end
 
   end
