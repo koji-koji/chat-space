90fc4c0f76dc526f9f206321d8ed07bd3ba39429
koji-koji <k.koyama37@gmail.com>
2017-06-23 19:46:18 +0900

    テストコードの記述

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,30 +3,83 @@ require 'rails_helper'
 describe CommentsController do
   describe 'GET #index' do
     let(:user) { create(:user)}
-    let(:group) { create(:chatgroup)}
-    let(:comments) { create(:comments)}
+    let(:chatgroup) { create(:chatgroup)}
+    let(:comment) { create(:comment)}
+    let(:comments) {create_list(:comment, 3)}
+
     context 'user log_in' do
       before do
         login_user user
       end
 
       it "assigns the requested comment to @comment" do
-        comment = create(:comment)
-        get :index, id: comment
-        expect(assigns(:comment)).to eq comment
+        get :index, params: {chatgroup_id: chatgroup[:id]}
+        expect(assigns(:comment)).to be_an_instance_of Comment
       end
 
+      it "assigns the requested comment to @comments" do
+        get :index, params: {chatgroup_id: chatgroup[:id]}
+        expect(assigns(:comments)).to match(comments.sort{ |a, b| a.created_at <=> b.created_at })
+      end
 
       it "renders the :index template" do
-        get :index, id: comment
-        # id: 1は渡すパラメータのハッシュ
+        get :index, params: {chatgroup_id: chatgroup[:id]}
         expect(response).to render_template :index
       end
     end
+
+    context 'user not log_in' do
+      it "redirect to user_session_path" do
+        get :index, params: {chatgroup_id: chatgroup[:id]}
+        expect(response).to redirect_to user_session_path
+      end
+    end
   end
-  # describe 'POST #create' do
-  #   it "assigns the requested comment to @comment"
-  #   expect(assigns(:commetn)).to eq comment
-  # end
+  describe 'POST #create' do
+    let(:user) { create(:user)}
+    let(:chatgroup) { create(:chatgroup)}
+    let(:comment) { create(:comment)}
+
+    context 'user log_in and save success' do
+      before do
+        login_user user
+      end
+
+      it 'saves the posted text to @comment ' do
+        test_comment = comment.slice(:comment)
+        expect{post :create, params: {chatgroup_id: chatgroup[:id],comment: test_comment, user_id: comment[:id],image: comment[:image]}}.to change(Comment, :count).by(1)
+      end
+
+
+      it 'redirect_to chatgroup_comments_path' do
+        test_comment = comment.slice(:comment)
+        post :create, params: {chatgroup_id: chatgroup[:id],comment: test_comment, user_id: comment[:id],image: comment[:image]}
+        expect(response).to redirect_to chatgroup_comments_path
+      end
+    end
+
+    context 'user log_in but save failed' do
+      before do
+        login_user user
+      end
+
+      it 'does not saves the posted text to @comment ' do
+        expect{post :create, params: {chatgroup_id: chatgroup[:id],comment: {comment: "", image: nil}, chatgroup_id: chatgroup.id}}.not_to change(Comment, :count)
+      end
+
+      it 'does not saves the posted and redirect_to chatgroup_comments_path' do
+        test_comment = comment.slice(:comment)
+        post :create, params: {chatgroup_id: chatgroup[:id],comment: test_comment, user_id: comment[:id],image: comment[:image]}
+        expect(response).to redirect_to chatgroup_comments_path
+      end
+    end
+
+    context 'user not log_in' do
+      it "redirect to user_session_path" do
+        get :index, params: {chatgroup_id: chatgroup[:id]}
+        expect(response).to redirect_to user_session_path
+      end
+    end
+  end
 
 end
