5091f129065a6dc0fe3bba45c0b706430c4f9478
koji-koji <k.koyama37@gmail.com>
2017-06-23 22:01:59 +0900

    factory chatgroupの調整
    ・グループ名が少なすぎて、名前が重複し不要なエラーが出ていたため、数の多いfakerを設定した。
    ・created_atとupdated_atが順序通りになるように調整をした。

--- spec/factories/chatgroup.rb
+++ spec/factories/chatgroup.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
 
   factory :chatgroup do
-    group_name         {Faker::StarWars.droid}
-    created_at         {Faker::Time.between(2.days.ago, Date.today, :midnight)}
-    updated_at         {Faker::Time.between(2.days.ago, Date.today, :midnight)}
+    group_name         {Faker::Company.name}
+    created_at         Faker::Time.between(10.days.ago, 6.days.ago, :day)
+    updated_at         Faker::Time.between(5.days.ago , 2.days.ago, :day)
   end
 
 end
