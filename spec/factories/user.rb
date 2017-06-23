c4c7c926021e751afaf63bc6592788fe2bbc5504
koji-koji <k.koyama37@gmail.com>
2017-06-23 20:40:19 +0900

    パスワードの記述の調整

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,8 +3,9 @@ FactoryGirl.define do
   factory :user do
     email              {Faker::Internet.email}
     name               {Faker::Name.name }
-    password               "password"
-    password_confirmation  "password"
+    pass = Faker::Internet.password
+    password                  {pass}
+    password_confirmation     {pass}
   end
 
 end
