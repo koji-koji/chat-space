b2b16cb5572fc2fe5e11e7d260aab06fe0ceac69
koji-koji <k.koyama37@gmail.com>
2017-06-24 12:55:24 +0900

    factoryの記述の調整
    記述を統一してfakerには{　}をつけた

--- spec/factories/comment.rb
+++ spec/factories/comment.rb
@@ -5,8 +5,8 @@ FactoryGirl.define do
     chatgroup_id       { 1 }
     user_id            { 1 }
     image              { File.open("#{Rails.root}/test/fixtures/files/test.jpg") }
-    created_at         Faker::Time.between(10.days.ago, 6.days.ago, :day)
-    updated_at         Faker::Time.between(5.days.ago , 2.days.ago, :day)
+    created_at         { Faker::Time.between(10.days.ago, 6.days.ago, :day)}
+    updated_at         { Faker::Time.between(5.days.ago , 2.days.ago, :day)}
     association :chatgroup
     association :user
   end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,11 +2,11 @@ FactoryGirl.define do
 
   factory :user do
     pass = Faker::Internet.password
-    email              {Faker::Internet.email}
-    name               {Faker::Name.name }
+    email              { Faker::Internet.email }
+    name               { Faker::Name.name }
     password           pass
     password_confirmation  pass
-    created_at         Faker::Time.between(10.days.ago, 6.days.ago, :day)
-    updated_at         Faker::Time.between(5.days.ago , 2.days.ago, :day)
+    created_at         { Faker::Time.between(10.days.ago, 6.days.ago, :day)}
+    updated_at         { Faker::Time.between(5.days.ago , 2.days.ago, :day)}
   end
 end
