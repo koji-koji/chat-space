6aec16fe7686589cee580026dcd36e8878b3c923
koji-koji <k.koyama37@gmail.com>
2017-06-24 15:39:11 +0900

    ・attributes_forの導入 ・user_idの値の変更 ・テスト内容確認文の調整

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -4,32 +4,31 @@ describe CommentsController do
   describe 'GET #index' do
     let(:user) { create(:user)}
     let(:chatgroup) { create(:chatgroup)}
-    let(:comment) { create(:comment)}
     let(:comments) { create_list(:comment, 3, chatgroup_id: chatgroup.id)}
     before do
       get :index, params: {chatgroup_id: chatgroup.id}
     end
 
-    context 'user log_in' do
+    context 'when user log_in' do
       before do
         login_user user
         get :index, params: {chatgroup_id: chatgroup.id}
       end
 
-      it "assigns the requested comment to @comment" do
+      it "assign the requested comment to @comment" do
         expect(assigns(:comment)).to be_an_instance_of Comment
       end
 
-      it "assigns the requested comment to @comments" do
+      it "assign the requested comments to @comments" do
         expect(assigns(:comments)).to match(comments.sort{ |a, b| a.created_at <=> b.created_at })
       end
 
-      it "renders the :index template" do
+      it "render the :index template" do
         expect(response).to render_template :index
       end
     end
 
-    context 'user not log_in' do
+    context 'when user not log_in' do
       it "redirect to user_session_path" do
         expect(response).to redirect_to user_session_path
       end
@@ -39,30 +38,26 @@ describe CommentsController do
   describe 'POST #create' do
     let(:user) { create(:user)}
     let(:chatgroup) { create(:chatgroup)}
-    let(:comment) { create(:comment)}
-    before do
-      @test_comment = comment.slice(:comment,:image)
-    end
 
-    context 'user log_in and save success' do
+    context 'when user log_in and save success' do
       before do
         login_user user
       end
 
-      it 'saves the posted text to @comment ' do
+      it 'save the new comment ' do
         expect{
-          post :create, params: {chatgroup_id: chatgroup.id,comment: @test_comment, user_id: comment.user_id}
+          post :create, params: {chatgroup_id: chatgroup.id, comment: attributes_for(:comment) , user_id: user.id}
         }.to change(Comment, :count).by(1)
       end
 
-      it 'redirect_to chatgroup_comments_path' do
-        post :create, params: {chatgroup_id: chatgroup.id,comment: @test_comment, user_id: comment.user_id}
+      it 'redirect to chatgroup_comments_path' do
+        post :create, params: {chatgroup_id: chatgroup.id, comment: attributes_for(:comment), user_id: user.id }
         expect(response).to redirect_to chatgroup_comments_path
       end
     end
 
-    context 'user log_in but save failed' do
-      let(:fail_params) {{ chatgroup_id: chatgroup.id, comment: {comment: "", image: nil}, user_id: comment.user_id}}
+    context 'when user log in but save failed' do
+      let(:fail_params) {{ chatgroup_id: chatgroup.id, comment: {comment: "", image: nil}, user_id: user.id}}
       before do
         login_user user
       end
@@ -84,7 +79,7 @@ describe CommentsController do
       end
     end
 
-    context 'user not log_in' do
+    context 'when user not log_in' do
       it "redirect to user_session_path" do
         get :index, params: {chatgroup_id: chatgroup.id}
         expect(response).to redirect_to user_session_path
