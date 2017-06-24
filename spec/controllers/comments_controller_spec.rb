0a58c48e35d12f1ffb6febd3bb95fada119909dc
koji-koji <k.koyama37@gmail.com>
2017-06-24 12:58:20 +0900

    comments_controller_spec.rbの修正
    ・commentsにchatgroup_idを指定
    ・誤った内容の修正
    ・不要な記述の削除
    ・余分な空行の削除
    ・chatgroup_idの取得方法の変更
    chatgroup[:id]→chatgroup.id

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -5,16 +5,17 @@ describe CommentsController do
     let(:user) { create(:user)}
     let(:chatgroup) { create(:chatgroup)}
     let(:comment) { create(:comment)}
-    let(:comments) {create_list(:comment, 3)}
+    let(:comments) { create_list(:comment, 3, chatgroup_id: chatgroup.id)}
     before do
-      get :index, params: {chatgroup_id: chatgroup[:id]}
+      get :index, params: {chatgroup_id: chatgroup.id}
     end
 
     context 'user log_in' do
       before do
         login_user user
-        get :index, params: {chatgroup_id: chatgroup[:id]}
+        get :index, params: {chatgroup_id: chatgroup.id}
       end
+
 
       it "assigns the requested comment to @comment" do
         expect(assigns(:comment)).to be_an_instance_of Comment
@@ -49,13 +50,12 @@ describe CommentsController do
       end
 
       it 'saves the posted text to @comment ' do
-        test_comment = comment.slice(:comment,:image)
-        expect{post :create, params: {chatgroup_id: chatgroup[:id],comment: @test_comment, user_id: comment[:id]}}.to change(Comment, :count).by(1)
+        expect{post :create, params: {chatgroup_id: chatgroup.id,comment: @test_comment, user_id: comment.user_id}}.to change(Comment, :count).by(1)
       end
 
       it 'redirect_to chatgroup_comments_path' do
         test_comment = comment.slice(:comment,:image)
-        post :create, params: {chatgroup_id: chatgroup[:id],comment: @test_comment, user_id: comment[:id]}
+        post :create, params: {chatgroup_id: chatgroup.id,comment: @test_comment, user_id: comment.user_id}
         expect(response).to redirect_to chatgroup_comments_path
       end
     end
@@ -66,24 +66,23 @@ describe CommentsController do
       end
 
       it 'does not saves the posted text to @comment ' do
-        expect{post :create, params: {chatgroup_id: chatgroup[:id],comment: {comment: "", image: nil}, chatgroup_id: chatgroup.id}}.not_to change(Comment, :count)
+        expect{post :create, params: {chatgroup_id: chatgroup.id,comment: {comment: "", image: nil}, user_id: comment.user_id}}.not_to change(Comment, :count)
       end
 
       it 'show flash message ' do
-        post :create, params: {chatgroup_id: chatgroup[:id],comment: {comment: "", image: nil}, chatgroup_id: chatgroup.id}
+        post :create, params: {chatgroup_id: chatgroup.id,comment: {comment: "", image: nil}, user_id: comment.user_id}
         expect(flash.now[:alert]).to include("メッセージかイメージを入力して下さい")
       end
 
       it 'does not saves the posted and redirect_to chatgroup_comments_path' do
-
-        post :create, params: {chatgroup_id: chatgroup[:id],comment: @test_comment, user_id: comment[:id]}
+        post :create, params: {chatgroup_id: chatgroup.id,comment: @test_comment, user_id: comment.user_id}
         expect(response).to redirect_to chatgroup_comments_path
       end
     end
 
     context 'user not log_in' do
       it "redirect to user_session_path" do
-        get :index, params: {chatgroup_id: chatgroup[:id]}
+        get :index, params: {chatgroup_id: chatgroup.id}
         expect(response).to redirect_to user_session_path
       end
     end
